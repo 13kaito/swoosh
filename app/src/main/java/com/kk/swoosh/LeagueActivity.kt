11916c9ce033b090eb4783a9fc414ea3851c5ba6
kennyK13 <kenny.kenrick@hotmail.com>
2019-07-07 10:17:22 +0800

    lesson-05

--- app/src/main/java/com/kk/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/kk/swoosh/LeagueActivity.kt
@@ -1,12 +1,21 @@
 package com.kk.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+    }
+
+    fun LeagueNextClicked(view : View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
--- app/src/main/java/com/kk/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/kk/swoosh/WelcomeActivity.kt
@@ -2,17 +2,16 @@ package com.kk.swoosh
 
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        getStartedBtn.setOnClickListener{
+        FinishedBtn.setOnClickListener{
             val leagueIntent = Intent(this,LeagueActivity::class.java)
             startActivity(leagueIntent)
         }
